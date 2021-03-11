ccba401f4e7caa2187cc505723b92d96aec9179e
nyappy15ch <69615045+nyappy15ch@users.noreply.github.com>
2021-03-11 12:36:54 +0900

    feat:webhookのリクエストが通るように設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,3 @@
-require 'line/bot'
-
 class ApplicationController < ActionController::API
-  protect_from_forgery with: :exception
 
-  def client
-    @client ||= Line::Bot::Client.new { |config|
-      config.channel_id = ENV["LINE_CHANNEL_ID"]
-      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-    }
-  end
 end
--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,7 +1,18 @@
+require 'line/bot'
+
 class ScoresController < ApplicationController
+  # protect_from_forgery with: :exception
+
+  def client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_id = ENV["LINE_CHANNEL_ID"]
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
+  end
 
 
-  post '/callback' do
+  def callback
     body = request.body.read
   
     signature = request.env['HTTP_X_LINE_SIGNATURE']
